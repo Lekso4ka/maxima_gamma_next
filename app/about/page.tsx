e19784e2f0a7e393190640d7e664b38de6af1416
surnachevaad <Aleksandra.Surnacheva@lukoil.com>
2025-06-07 12:05:37 +0300

    Start next js

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return <>
+        <h1>О нас</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius nihil temporibus velit!
+            Accusamus accusantium aliquam delectus, dolor, eligendi et eum facere harum illum natus
+            necessitatibus nemo sed sequi temporibus totam.</p>
+    </>
+}
